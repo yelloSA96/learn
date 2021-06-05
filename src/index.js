58738e8d30bb905b4376bf43a09af1b67496ee9c
yellosa96 <t.suebwicha@gmail.com>
2021-06-05 14:49:32 +0930

    Modified the file structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './css/index.css';
 import reportWebVitals from './reportWebVitals';
-import Routing from './Routing';
+import Routing from './components/Routing';
 
 ReactDOM.render(
   <React.StrictMode>
