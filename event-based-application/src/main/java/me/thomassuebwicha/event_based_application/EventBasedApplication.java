b1fb061c2385709fc59212756f5ef549ecbbd14e
yellosa96 <t.suebwicha@gmail.com>
2025-11-24 21:47:08 +1100

    Initial commit of event-based-spring application

---
+++ event-based-application/src/main/java/me/thomassuebwicha/event_based_application/EventBasedApplication.java
@@ -0,0 +1,13 @@
+package me.thomassuebwicha.event_based_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventBasedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventBasedApplication.class, args);
+	}
+
+}
